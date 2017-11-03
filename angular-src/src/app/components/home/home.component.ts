5763a0512eb6547c3d9c6513a7dec41c05a7a213
NeoBanana <33353794+NeoBanana@users.noreply.github.com>
2017-11-03 23:46:22 +0100

    Update home.component.ts

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     class:'md-g md-g--mobile--childs '
   },
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
