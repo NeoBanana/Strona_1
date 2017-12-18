adddddea4876a3fab204934c650ffbfc11f848d0
Kacper Kruczek <neronim66@gmail.com>
2017-12-18 01:31:48 +0100

    transfer of services
    remove unnecessary services load in app.module
    and 
    puts them in sharedModule.forRoot()

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -11,48 +11,29 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
+
 import { FrameModule } from './frame/frame.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { InvalidRouteComponent } from './invalid-route/invalid-route.component';
 
-import { EditionsService } from 'app/shared/services/connect/data/editions/editions.service';
-import { SponsorsService } from 'app/shared/services/connect/data/sponsors/sponsors.service';
-import { ComitteesService } from 'app/shared/services/connect/data/comittees/comittees.service';
-import { SchoolsService } from './shared/services/connect/data/schools/schools.service';
-
-
-import { ApperanceService } from 'app/shared/components/layout/services/apperance.service';
-import { MainProgressBarService } from './shared/services/progressbars/main-progress-bar.service';
-
-import { SnackbarService } from 'app/shared/services/messages/snackbar.service';
-import { ErrorService } from 'app/shared/services/connect/error.service';
-
 @NgModule({
   declarations: [
     AppComponent, 
     InvalidRouteComponent,
-
-
   ],
   imports: [
     FrameModule,
     BrowserAnimationsModule,
     HttpModule,
     HttpClientModule,
-    SharedModule,
+    SharedModule.forRoot(),
     AppRoutingModule,
     HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false })
+      InMemoryDataService, { dataEncapsulation: false,
+      delay:1500 })
   ],
   providers: [
-    EditionsService,
-    ComitteesService,
-     SnackbarService,   
-      ErrorService,
-      MainProgressBarService,
-      ApperanceService,
-      SchoolsService
   ],
   bootstrap: [AppComponent]
 })
--- angular-src/src/app/shared/shared.module.ts
+++ angular-src/src/app/shared/shared.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from './modules/material.module';
-import { WidgetsModule } from './components/widgets/widgets.module';
-import { FormsModule,ReactiveFormsModule  } from '@angular/forms'; 
-import { RouterModule, Routes} from '@angular/router';
-import { LayoutComponent } from './components/layout/layout.component';
-import { LoginElement } from 'app/shared/components/login/login.component';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
 
+import { MaterialModule } from './modules/material.module';
+
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+// Layout
+import { LayoutComponent } from './components/layout/layout.component';
+import { WidgetsModule } from './components/widgets/widgets.module';
+
+// Data Services
+import { EditionsService } from 'app/shared/services/connect/data/editions/editions.service';
+import { SponsorsService } from 'app/shared/services/connect/data/sponsors/sponsors.service';
+import { ComitteesService } from 'app/shared/services/connect/data/comittees/comittees.service';
+import { SchoolsService } from 'app/shared/services/connect/data/schools/schools.service';
+
+// Apperance Services
+import { ApperanceService } from 'app/shared/components/layout/services/apperance.service';
+import { MainProgressBarService } from 'app/shared/services/progressbars/main-progress-bar.service';
+
+// Popup Services
+import { SnackbarService } from 'app/shared/services/messages/snackbar.service';
+import { ErrorService } from 'app/shared/services/connect/error.service';
+
+console.log("loading shared");
 @NgModule({
   imports: [
     CommonModule,
@@ -15,17 +35,36 @@ import { LoginElement } from 'app/shared/components/login/login.component';
     WidgetsModule,
     FormsModule,
     ReactiveFormsModule,
-   
+
   ],
-  exports:[
+  exports: [
     MaterialModule,
     WidgetsModule,
     RouterModule,
     FormsModule,
     ReactiveFormsModule,
     LayoutComponent,
-    LoginElement
   ],
-  declarations: [LayoutComponent,LoginElement ]  
+  declarations: [LayoutComponent]
 })
-export class SharedModule { }
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [
+        EditionsService,
+        ComitteesService,
+        SnackbarService,
+        ErrorService,
+        MainProgressBarService,
+        ApperanceService,
+        SchoolsService]
+    }
+  }
+  static forChild(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    }
+  }
+}
